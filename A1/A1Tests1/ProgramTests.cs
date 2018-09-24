e0c6670f20678baf104aba2c052af0481f0db424
Ali.reza1044 <nili_505077@yahoo.com>
2018-09-24 23:20:45 +0330

    Finished

---
+++ A1/A1Tests1/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(3, Program.Add(1, 2));
+        }
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem("TestData", "TestData")]
+        public void GradedTest()
+        {
+            TestTools.RunLocalTest(Program.Process);
+        }
+    }
+}
