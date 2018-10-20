d0d3518d8218b695ddf1877d6cc159f7d8f51c21
Ali.reza1044 <nili_505077@yahoo.com>
2018-10-20 19:18:06 +0330

    Basic

---
+++ A5/A5/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A5
+{
+    public class Program
+    {
+        static void Main(string[] args) { }
+
+        public static long[] BinarySearch1(long[] a , long [] b)
+        {
+            //write your code here
+            return new long[] { 0 };
+        }
+
+        public static string ProcessBinarySearch1(string inStr) =>
+            TestTools.Process(inStr, BinarySearch1);
+
+
+
+        public static long MajorityElement2(long n, long[] a)
+        {
+            //write your code here
+            return 0;
+        }
+
+        public static string ProcessMajorityElement2(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)MajorityElement2);
+
+        public static long[] ImprovingQuickSort3(long n, long[] a)
+        {
+            //write your code here          
+            return new long[] { 0 };
+        }
+
+        public static string ProcessImprovingQuickSort3(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long[]>)ImprovingQuickSort3);
+
+        public static long NumberofInversions4(long n, long[] a)
+        {
+            //write your code here
+            return 0;
+        }
+
+        public static string ProcessNumberofInversions4(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)NumberofInversions4);
+
+        public static long[] OrganizingLottery5(long[] points, long[] startSegments,
+            long[] endSegment)
+        {
+            //write your code here
+            return new long[] { 0 };
+        }
+
+        public static string ProcessOrganizingLottery5(string inStr) =>
+            TestTools.Process(inStr,OrganizingLottery5);
+
+        public static double ClosestPoints6(long n, long[] xPoints, long[] yPoints)
+        {
+            //write your code here
+            return 0;
+        }
+
+        public static string ProcessClosestPoints6(string inStr) =>
+           TestTools.Process(inStr, (Func<long, long[], long[], double>)
+               ClosestPoints6);
+
+    }
+}
---
+++ A5/A5Tests/ProgramTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A5.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod(),Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_BinarySearch1Test()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessBinarySearch1, "TD1");
+        }
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_MajorityElement2Test()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessMajorityElement2, "TD2");
+        }
+
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_ImprovingQuickSort3Test()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessImprovingQuickSort3, "TD3");
+        }
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_NumberofInversions4Test()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessNumberofInversions4, "TD4");
+        }
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_OrganizingLottery5Test()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessOrganizingLottery5, "TD5");
+        }
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A5_TestData")]
+        public void Graded_ClosestPoints6()
+        {
+            TestTools.RunLocalTest("A5", Program.ProcessClosestPoints6, "TD6");
+        }
+
+    }
+}
