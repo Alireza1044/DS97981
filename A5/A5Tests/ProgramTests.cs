467bd049861cbc547d51110b018f84eb40af9234
Ali.reza1044 <nili_505077@yahoo.com>
2018-10-31 19:55:15 +0330

    Done

--- A5/A5Tests/ProgramTests.cs
+++ A5/A5Tests/ProgramTests.cs
@@ -19,7 +19,7 @@ namespace A5.Tests
             TestTools.RunLocalTest("A5", Program.ProcessBinarySearch1, "TD1");
         }
 
-        [TestMethod(), Timeout(1000)]
+        [TestMethod(),Timeout(1000)]
         [DeploymentItem(@"TestData", "A5_TestData")]
         public void Graded_MajorityElement2Test()
         {
@@ -27,7 +27,7 @@ namespace A5.Tests
         }
 
 
-        [TestMethod(), Timeout(1000)]
+        [TestMethod()]
         [DeploymentItem(@"TestData", "A5_TestData")]
         public void Graded_ImprovingQuickSort3Test()
         {
@@ -41,14 +41,14 @@ namespace A5.Tests
             TestTools.RunLocalTest("A5", Program.ProcessNumberofInversions4, "TD4");
         }
 
-        [TestMethod(), Timeout(1000)]
+        [TestMethod()]
         [DeploymentItem(@"TestData", "A5_TestData")]
         public void Graded_OrganizingLottery5Test()
         {
             TestTools.RunLocalTest("A5", Program.ProcessOrganizingLottery5, "TD5");
         }
 
-        [TestMethod(), Timeout(1000)]
+        [TestMethod()]
         [DeploymentItem(@"TestData", "A5_TestData")]
         public void Graded_ClosestPoints6()
         {
