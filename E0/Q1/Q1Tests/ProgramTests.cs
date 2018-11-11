5378360c2914cc85326f761eb239179f891cf377
Ali.reza1044 <nili_505077@yahoo.com>
2018-11-11 18:03:16 +0330

    added e0 test

---
+++ E0/Q1/Q1Tests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Q1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+        }
+    }
+}
