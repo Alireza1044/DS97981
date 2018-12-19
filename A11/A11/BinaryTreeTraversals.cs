51f2cb269de7f9f677adf53c7223c84d36acc55e
Ali.reza1044 <nili_505077@yahoo.com>
2018-12-19 18:07:49 +0330

    inorder almost done

--- A11/A11/BinaryTreeTraversals.cs
+++ A11/A11/BinaryTreeTraversals.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using TestCommon;
 
 namespace A11
 {
+    public class Node
+    {
+        public int LeftChild { get; set; }
+        public int RightChild { get; set; }
+        public int Parent { get; set; }
+        public int Key { get; set; }
+        public Node(int key, int left, int right)
+        {
+            Key = key;
+            LeftChild = left;
+            RightChild = right;
+        }
+    }
     public class BinaryTreeTraversals : Processor
     {
         public BinaryTreeTraversals(string testDataName) : base(testDataName) { }
@@ -13,7 +27,56 @@ namespace A11
 
         public long[][] Solve(long[][] nodes)
         {
-            return new long[][] { };
+            Node[] Tree = new Node[nodes.GetLength(0)];
+            List<long[]> result = new List<long[]>();
+
+            for (int i = 0; i < Tree.Length; i++)
+            {
+                Tree[i] = new Node(-1, -1, -1);
+            }
+
+            for (int i = 0; i < nodes.GetLength(0); i++)
+            {
+                Tree[i].Key = (int)nodes[i][0];
+                Tree[i].LeftChild = (int)nodes[i][1];
+                Tree[i].RightChild = (int)nodes[i][2];
+                if (Tree[i].LeftChild != -1)
+                    Tree[(int)nodes[i][1]].Parent = i;
+                if (Tree[i].RightChild != -1)
+                    Tree[(int)nodes[i][2]].Parent = i;
+            weresult.Add(PreOrder(Tree));
+            return result.ToArray();
+        }
+
+        public static long[] PreOrder(Node[] Tree)
+        {
+            Stack<Node> nodePile = new Stack<Node>();
+            Node current = Tree[0];
+            List<long> result = new List<long>();
+
+            while (current.Key != -1)
+            {
+                nodePile.Push(current);
+                current = Tree[current.LeftChild];
+            }
+
+            while (true)
+            {
+                if(current.LeftChild == -1 && nodePile.Count != 0)
+                {
+                    result.Add(nodePile.Peek().Key);
+                    current = Tree[nodePile.Pop().RightChild];
+                    while (current.Key != -1)
+                    {
+                        nodePile.Push(current);
+                        current = Tree[current.LeftChild];
+                    }
+                }
+                if (current.LeftChild == -1 && nodePile.Count == 0)
+                {
+                    return result.ToArray();
+                }
+            }
         }
     }
 }
--- A11/A11/Program.cs
+++ A11/A11/Program.cs
@@ -10,6 +10,28 @@ namespace A11
     {
         public static void Main(string[] args)
         {
+            BinaryTreeTraversals b = new BinaryTreeTraversals("a1");
+            List<long[]> node = new List<long[]>();
+            List<long> z = new List<long>();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    z.Add(long.Parse(Console.ReadLine()));
+                }
+                node.Add(z.ToArray());
+                z = new List<long>();
+            }
+            long[][] s = b.Solve(node.ToArray());
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(s[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
