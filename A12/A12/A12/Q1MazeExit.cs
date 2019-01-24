6cfe192bd39f0989f2700f3f07369dd8df8ecbbe
Ali.reza1044 <nili_505077@yahoo.com>
2019-01-25 01:33:48 +0330

    1 to 4 done, 5 still needs work!

---
+++ A12/A12/A12/Q1MazeExit.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A12
+{
+    public class Edge
+    {
+        public List<long> Connected { get; set; }
+        public List<long> Parents { get; set; }
+        public List<long> ConnectedReverse { get; set; }
+        public List<long> ParentsReverse { get; set; }
+        public long Key { get; set; }
+        public long InDegree { get; set; }
+        public bool IsVisited { get; set; }
+        public Edge(long key)
+        {
+            Key = key;
+            IsVisited = false;
+            InDegree = 0;
+            Connected = new List<long>();
+            Parents = new List<long>();
+            ConnectedReverse = new List<long>(); ;
+            ParentsReverse = new List<long>();
+        }
+    }
+
+    public class Q1MazeExit : Processor
+    {
+        public Q1MazeExit(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
+        {
+            Edge[] Graph = new Edge[nodeCount+1];
+            for (int i = 1; i < Graph.Length; i++)
+            {
+                Graph[i] = new Edge(i);
+            }
+            for (int i = 0; i < edges.GetLength(0); i++)
+            {
+                for (int j = 0; j < edges[i].Length; j++)
+                {
+                    if (!Graph[edges[i][0]].Connected.Contains(edges[i][1]))
+                        Graph[edges[i][0]].Connected.Add(edges[i][1]);
+                    if (!Graph[edges[i][1]].Connected.Contains(edges[i][0]))
+                        Graph[edges[i][1]].Connected.Add(edges[i][0]);
+                }
+            }
+            return DFS(Graph,edges,StartNode,EndNode);
+        }
+        
+        public int DFS(Edge[] graph,long[][]edges,long startnode,long endNode)
+        {
+            Queue<long> queue = new Queue<long>();
+            queue.Enqueue(graph[startnode].Key);
+            graph[startnode].IsVisited = true;
+            while (queue.Count > 0)
+            {
+                long temp = queue.Dequeue();
+                for (int i = 0; i < graph[temp].Connected.Count; i++)
+                {
+                    if (!graph[graph[temp].Connected[i]].IsVisited)
+                    {
+                        queue.Enqueue(graph[temp].Connected[i]);
+                        graph[graph[temp].Connected[i]].IsVisited = true;
+                    }
+                }
+                if (queue.Contains(endNode))
+                    return 1;
+            }
+            return 0;
+        }   
+     }
+}
---
+++ A12/A12/A12/Q4OrderOfCourse.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TestCommon;
+
+namespace A12
+{
+    public class Q4OrderOfCourse: Processor
+    {
+        public Q4OrderOfCourse(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long[]>)Solve);
+
+        public long[] Solve(long nodeCount, long[][] edges)
+        {
+            Edge[] Graph = new Edge[nodeCount + 1];
+            for (int i = 1; i < Graph.Length; i++)
+            {
+                Graph[i] = new Edge(i);
+            }
+            //build the graph
+            for (int i = 0; i < edges.GetLength(0); i++)
+            {
+                for (int j = 0; j < edges[i].Length; j++)
+                {
+                    if (!Graph[edges[i][0]].Connected.Contains(edges[i][1]))
+                    {
+                        Graph[edges[i][0]].Connected.Add(edges[i][1]);
+                        Graph[edges[i][1]].InDegree++;
+                    }
+
+                    if (!Graph[edges[i][1]].Parents.Contains(edges[i][0]))
+                    {
+                        Graph[edges[i][1]].Parents.Add(edges[i][0]);
+                    }
+                }
+            }
+            var stack = DFS(Graph, edges, nodeCount);
+            List<long> result = new List<long>();
+            while(stack.Count != 0)
+            {
+                result.Add(stack.Pop());
+            }
+            result.Reverse();
+            return result.ToArray();
+        }
+
+        public Stack<long> DFS(Edge[] graph, long[][] edges, long nodeCount)
+        {
+            long visitedCount = 0;
+            Stack<long> stack = new Stack<long>();
+            Queue<long> queue = new Queue<long>();
+            for (int i = graph.Length-1; i > 0; i--)
+            {
+                if (graph[i].InDegree == 0)
+                    queue.Enqueue(graph[i].Key);
+            }
+
+            while (queue.Count > 0)
+            {
+                long temp = queue.Dequeue();
+                stack.Push(temp);
+                visitedCount++;
+                for (int i = 0; i < graph[temp].Connected.Count; i++)
+                {
+                    graph[graph[temp].Connected[i]].InDegree--;
+                    if (graph[graph[temp].Connected[i]].InDegree == 0)
+                        queue.Enqueue(graph[temp].Connected[i]);
+                }
+            }
+
+            return stack;
+        }
+
+        public override Action<string, string> Verifier { get; set; } = TopSortVerifier;
+
+        /// <summary>
+        /// کد شما با متد زیر راست آزمایی میشود
+        /// این کد نباید تغییر کند
+        /// داده آزمایشی فقط یک جواب درست است
+        /// تنها جواب درست نیست
+        /// </summary>
+        public static void TopSortVerifier(string inFileName, string strResult)
+        {
+            long[] topOrder = strResult.Split(TestTools.IgnoreChars)
+                .Select(x => long.Parse(x)).ToArray();
+
+            long count;
+            long[][] edges;
+            TestTools.ParseGraph(File.ReadAllText(inFileName), out count, out edges);
+
+            // Build an array for looking up the position of each node in topological order
+            // for example if topological order is 2 3 4 1, topOrderPositions[2] = 0, 
+            // because 2 is first in topological order.
+            long[] topOrderPositions = new long[count];
+            for (int i = 0; i < topOrder.Length; i++)
+                topOrderPositions[topOrder[i] - 1] = i;
+            // Top Order nodes is 1 based (not zero based).
+
+            // Make sure all direct depedencies (edges) of the graph are met:
+            //   For all directed edges u -> v, u appears before v in the list
+            foreach (var edge in edges)
+                if (topOrderPositions[edge[0] - 1] >= topOrderPositions[edge[1] - 1])
+                    throw new InvalidDataException(
+                        $"{Path.GetFileName(inFileName)}: " +
+                        $"Edge dependency violoation: {edge[0]}->{edge[1]}");
+
+        }
+    }
+}
