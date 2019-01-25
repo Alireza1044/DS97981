1a5e29d4bace11c028be1fab0064bc2c96264d2a
Ali.reza1044 <nili_505077@yahoo.com>
2019-01-25 20:31:04 +0330

    ...

--- A11/A11Tests/GradedTests.cs
+++ A11/A11Tests/GradedTests.cs
@@ -17,11 +17,11 @@ namespace A11.Tests
         public void SolveTest()
         {
             Processor[] problems = new Processor[] {
-                new BinaryTreeTraversals("TD1"),
-                new IsItBST("TD2"),
-                new IsItBSTHard("TD3"),
-                new SetWithRageSums("TD4"),
-                //new Rope("TD5")
+                //new BinaryTreeTraversals("TD1"),
+                //new IsItBST("TD2"),
+                //new IsItBSTHard("TD3"),
+                //new SetWithRageSums("TD4"),
+                new Rope("TD5")
             };
 
             foreach (var p in problems)
