dbe37ba85544fb33f24aebdff72edf7e4783828a
Alireza Moradi <nili_505077@yahoo.com>
2019-02-03 08:28:03 +0000

    Updated GradedTests.cs

--- A11/A11Tests/GradedTests.cs
+++ A11/A11Tests/GradedTests.cs
@@ -12,16 +12,16 @@ namespace A11.Tests
     [TestClass()]//Grade:A11:0
     public class GradedTests
     {
-        [TestMethod(),Timeout(12000)]//Timeout???
+        [TestMethod(),Timeout(10000)]//Timeout???
         [DeploymentItem("TestData", "A11_TestData")]
         public void SolveTest()
         {
             Processor[] problems = new Processor[] {
-                //new BinaryTreeTraversals("TD1"),
-                //new IsItBST("TD2"),
-                //new IsItBSTHard("TD3"),
-                //new SetWithRageSums("TD4"),
-                new Rope("TD5")
+                new BinaryTreeTraversals("TD1"),
+                new IsItBST("TD2"),
+                new IsItBSTHard("TD3"),
+                new SetWithRageSums("TD4"),
+                //new Rope("TD5")
             };
 
             foreach (var p in problems)
